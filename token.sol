f1142f3c88f50b8640b496dd8c2c2e19a43160e3
Diego armando Colque <colquedcr@gmail.com>
2025-06-22 19:23:17 -0300

    Add files via upload

---
+++ token.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor(string memory name, string memory sym)
+     ERC20(name, sym) {}
+
+     function mint (address to, uint256 amount) public { 
+         _mint(to, amount);   
+        
+    }
+  
+   
+}
